02e55b6c0ca9c28c4f46297292a0559fa05634d4
nadersameh_ <naders@cypodsolutions.com>
2025-08-03 15:16:27 +0300

    Add build dir

---
+++ zephyr/samples/hello_world/build/image_preload.cmake
@@ -0,0 +1,7 @@
+# Generated file that can be used to preload variant images
+set(BOARD "qemu_cortex_m3/ti_lm3s6965" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_ROOT "d:/ncs-d/cylockv2i7.2;d:/ncs-d/cytrackerv1.1/cylockv2i7.2;d:/ncs-d/thurayagateway;d:/ncs-d/beacon-tag;d:/ncs-d/v2.7.0-rc3/zephyr/samples/hello_world" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "D:/ncs-D/v2.7.0-rc3/zephyr/samples/hello_world/boards/qemu_cortex_m3.overlay" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "D:/ncs-D/toolchains/ce3b5ff664/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
---
+++ zephyr/samples/hello_world/build/zephyr/include/generated/ncs_commit.h
@@ -0,0 +1,12 @@
+#ifndef _NCS_COMMIT_H_
+#define _NCS_COMMIT_H_
+
+/*  values come from cmake/version.cmake
+ * BUILD_COMMIT related  values will be 'git rev-parse',
+ * alternatively user defined BUILD_VERSION.
+ */
+
+#define NCS_COMMIT                   cb7b6efdf17a
+#define NCS_COMMIT_STRING            "cb7b6efdf17a"
+
+#endif /* _NCS_COMMIT_H_ */
